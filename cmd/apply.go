2568326c72f8ee55c15ffdd9c68ebe10c72b9e7d
mishamyrt <misha@myrt.co>
2024-12-21 14:10:51 +0300

    feat: improve logging

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,13 +1,14 @@
 package cmd
 
 import (
+	"github.com/mishamyrt/ticketeer/internal/config"
 	"github.com/mishamyrt/ticketeer/internal/ticketeer"
 	"github.com/spf13/cobra"
 )
 
 type apply struct{}
 
-func (apply) New(opts *ticketeer.Options) *cobra.Command {
+func (apply) New(app *ticketeer.App) *cobra.Command {
 	var applyArgs ticketeer.ApplyArgs
 
 	applyCmd := cobra.Command{
@@ -15,13 +16,18 @@ func (apply) New(opts *ticketeer.Options) *cobra.Command {
 		Short:   "Append ticket id to commit message",
 		Example: "ticketeer apply",
 		RunE: func(_ *cobra.Command, _ []string) error {
-			return ticketeer.Apply(opts, &applyArgs)
+			return app.Apply(&applyArgs)
 		},
 	}
 
 	applyCmd.Flags().StringVar(
 		&applyArgs.DryRunWith, "dry-run-with", "",
 		"skip reading and writing message, use fake commit message",
+	)
+
+	applyCmd.Flags().StringVarP(
+		&applyArgs.ConfigPath, "config", "c", config.DefaultPath,
+		"path to configuration file",
 	)
 
 	return &applyCmd
